b6d288b13f0e20b44671535c8744efc7045a0cec
Daneyon Hansen <daneyonhansen@gmail.com>
2019-04-25 13:27:18 -0700

    Temporary

---
+++ hack/release-local.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -euo pipefail
+
+REPO="${REPO:-}"
+MANIFESTS="${MANIFESTS:-}"
+
+if [ -z "$REPO" ]; then echo "REPO is required"; exit 1; fi
+if [ -z "$MANIFESTS" ]; then echo "MANIFESTS is required"; exit 1; fi
+
+TEMP_COMMIT="false"
+test -z "$(git status --porcelain)" || TEMP_COMMIT="true"
+
+if [[ "${TEMP_COMMIT}" == "true" ]]; then
+  git add .
+  git commit -m "Temporary" || true
+fi
+
+REV=$(git rev-parse --short HEAD)
+TAG="${TAG:-$REV}"
+
+if [[ -z "${DOCKER+1}" ]] && command -v buildah >& /dev/null; then
+  buildah bud -t $REPO:$TAG .
+  buildah push $REPO:$TAG docker://$REPO:$TAG
+else
+  docker build -t $REPO:$TAG .
+  docker push $REPO:$TAG
+fi
+
+if [[ "${TEMP_COMMIT}" == "true" ]]; then
+  git reset --soft HEAD~1
+fi
+
+cp -R manifests/* $MANIFESTS
+cat manifests/02-deployment.yaml | sed "s~openshift/origin-cluster-externaldns-operator:latest~$REPO:$TAG~" > "$MANIFESTS/02-deployment.yaml"
+
+echo "Pushed $REPO:$TAG"
+echo "Install manifests using:"
+echo ""
+echo "oc apply -f $MANIFESTS"
+echo ""
+echo "Alternatively, rollout just a new operator deployment with:"
+echo ""
+echo "oc apply -f $MANIFESTS/02-deployment.yaml"
